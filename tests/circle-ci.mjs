4a830e4915c02f1894b40a60389f531940e61de4
αηdr3α5 ɮαηɦօlʐ3ʀ <AndreasBanholzer@Me.com>
2024-01-23 14:35:12 +0100

    update circle ci test (#3)

--- tests/circle-ci.mjs
+++ tests/circle-ci.mjs
@@ -2,7 +2,9 @@ import { EvmNodes } from '../src/EvmNodes.mjs'
 
 const evmNodes = new EvmNodes()
 if( evmNodes.health() ) {
+    console.log( 'Success!' )
     process.exit( 0 )
 } else {
+    console.log( 'Failure!' )
     process.exit( 1 )
 }
